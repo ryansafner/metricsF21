ccc9596676a7df744d1fce5a46be0b07b53f0f5e
Ryan Safner <ryansafner@gmail.com>
2021-10-28 10:59:44 -0400

    3.4 slides tweak

--- static/slides/libs/kePrint/kePrint.js
+++ static/slides/libs/kePrint/kePrint.js
@@ -6,11 +6,3 @@ $(document).ready(function(){
         $('[data-toggle="popover"]').popover();
     }
 });
-$(document).ready(function(){
-    if (typeof $('[data-toggle="tooltip"]').tooltip === 'function') {
-        $('[data-toggle="tooltip"]').tooltip();
-    }
-    if ($('[data-toggle="popover"]').popover === 'function') {
-        $('[data-toggle="popover"]').popover();
-    }
-});
